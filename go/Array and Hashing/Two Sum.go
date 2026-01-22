ddc9651fea2f806b4230286f730124cc681b789b
Ash007-kali <abhishek.wasnik24@gmail.com>
2026-01-22 11:07:15 +0530

    two sum

--- go/Array and Hashing/Two Sum.go
+++ go/Array and Hashing/Two Sum.go
@@ -1,1 +1,18 @@
 package main
+
+func twoSum(nums []int, target int) []int {
+
+	counter := make(map[int]int)
+
+	for i, val := range nums {
+
+		diff := target - val
+		if idx, exists := counter[diff]; exists {
+			return []int{idx, i}
+		}
+
+		counter[val] = i
+	}
+
+	return []int{}
+}
